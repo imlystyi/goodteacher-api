19c9bfdc550d4d30368a1e620f6db26c3ab4fd0e
Vladyslav Yakubovskyi <47981548+imlystyi@users.noreply.github.com>
2023-11-15 22:29:33 +0200

    Re-init

---
+++ src/main/java/com/goodteacher/api/TeacherAppApplication.java
@@ -0,0 +1,13 @@
+package com.goodteacher.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherAppApplication.class, args);
+	}
+
+}
