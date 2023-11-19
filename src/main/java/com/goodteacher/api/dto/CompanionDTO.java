d0d7abe67c8d2e951fce7616b3e422f46291d4df
Vladyslav Yakubovskyi <47981548+imlystyi@users.noreply.github.com>
2023-11-19 19:53:32 +0200

    Minor changes in CompanionDTO.java

--- src/main/java/com/goodteacher/api/dto/CompanionDTO.java
+++ src/main/java/com/goodteacher/api/dto/CompanionDTO.java
@@ -1,7 +1,14 @@
 package com.goodteacher.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanionDTO {
     private UUID id;
     private String name;
