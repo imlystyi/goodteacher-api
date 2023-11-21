ce7f7abd6dad3c7332995dc83c04390b070b9a79
slav41k <fifayaroslav@gmail.com>
2023-11-21 20:46:41 +0200

    added Group, GroupRepository, GroupService, GroupServiceImpl

--- src/main/java/com/goodteacher/api/service/impl/GroupServiceImpl.java
+++ src/main/java/com/goodteacher/api/service/impl/GroupServiceImpl.java
@@ -2,7 +2,7 @@ package com.goodteacher.api.service.impl;
 
 import com.goodteacher.api.entity.Group;
 import com.goodteacher.api.entity.Student;
-import com.goodteacher.api.repository.GroupRepository;
+
 
 public class GroupServiceImpl {
 
