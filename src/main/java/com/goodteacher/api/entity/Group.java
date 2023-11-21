384e7e47f0cd0f9ca162e3828cc67755ec8ad931
slav41k <fifayaroslav@gmail.com>
2023-11-21 20:44:58 +0200

    added Group, GroupRepository, GroupService, GroupServiceImpl

--- src/main/java/com/goodteacher/api/entity/Group.java
+++ src/main/java/com/goodteacher/api/entity/Group.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 import java.util.UUID;
 
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode()
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
