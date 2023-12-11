9891ecb94d89673bb329cf18bca747c42a818b1d
slav41k <115183875+slav41k@users.noreply.github.com>
2023-12-11 21:37:55 +0200

    Update Student.java
    added UserDetails

--- src/main/java/com/goodteacher/api/entity/Student.java
+++ src/main/java/com/goodteacher/api/entity/Student.java
@@ -2,20 +2,25 @@ package com.goodteacher.api.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 @Entity
+@EnableWebSecurity
 @Table(name = "students")
 @EqualsAndHashCode
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Student {
+public class Student implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -54,7 +59,42 @@ public class Student {
 
     @ManyToMany
     @JoinTable(name = "students_groups",
-               joinColumns = @JoinColumn(name = "student_id"),
-               inverseJoinColumns = @JoinColumn(name = "group_id"))
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id"))
     private List<Group> groups;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_STUDENT"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return nickname;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
 }
